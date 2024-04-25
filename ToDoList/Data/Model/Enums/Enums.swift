f6ac8afb5b94a6c632479c1f903aec756c6cf688
Adrián Iraizos Mendoza <86532545+airaizos@users.noreply.github.com>
2024-04-25 11:25:47 +0200

    DESIGN + FEAT: DetailTaskView DESIGN: Añadido BackgroundColorList depende del status de la tarea DESING: Cambiado Section por botón de Status y colocado al bottom DESIGN: Cambiada sección de createAt y changeAt a safeAreaInset bottom FEAT: Añadido botón de Cancelar, no afecta a las tags

--- ToDoList/Data/Model/Enums/Enums.swift
+++ ToDoList/Data/Model/Enums/Enums.swift
@@ -1,7 +1,17 @@
-import Foundation
+import SwiftUI
 
 enum TodoStatus: String, Codable {
     case pending
     case inProcess
     case completed
+}
+
+extension TodoStatus {
+    static func colorStyle(_ status: TodoStatus) -> Color {
+        switch status {
+        case .pending: Color.orange
+        case .inProcess: Color.yellow
+        case .completed: Color.green
+        }
+    }
 }
--- ToDoList/View/DetailTaskView.swift
+++ ToDoList/View/DetailTaskView.swift
@@ -2,7 +2,7 @@
 import SwiftUI
 
 struct DetailTaskView: View {
-    
+    @Environment(\.dismiss) var dismiss
     @Environment(TaskViewModel.self) var taskViewModel: TaskViewModel
     
     @State private var taskTitle: String = "Titulo..."
@@ -11,84 +11,107 @@ struct DetailTaskView: View {
     
     @State var task: TasksLocal
     @State var localTags: [TagLocal]
-    
+    @State var originalTask: TasksLocal? = nil
     var body: some View {
-        List {
-            Section("section_title") {
-                CustomTextField(
-                    placeholder: taskTitle,
-                    text: $task.title,
-                    onEditingChanged: { _ in
-                    })
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            
-            Section("section_tags") {
-                TagField(tags: $localTags)
-                    .onChange(of: localTags) { newTag, oldTag in
-                        if let tag = newTag.last, oldTag.last != nil {
-                            if newTag.count < oldTag.count {
-                                taskViewModel.addTag(title: tag.title, idTaskItem: task.id, idTag: tag.id)
-                            } else if newTag.count > oldTag.count {
-                                taskViewModel.removeOneTag(id: oldTag.last?.id ?? UUID())
+        VStack{
+            List {
+                Section("section_title") {
+                    CustomTextField(
+                        placeholder: taskTitle,
+                        text: $task.title,
+                        onEditingChanged: { _ in
+                        })
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                
+                Section("section_tags") {
+                    TagField(tags: $localTags)
+                        .onChange(of: localTags) { newTag, oldTag in
+                            if let tag = newTag.last, oldTag.last != nil {
+                                if newTag.count < oldTag.count {
+                                    taskViewModel.addTag(title: tag.title, idTaskItem: task.id, idTag: tag.id)
+                                } else if newTag.count > oldTag.count {
+                                    taskViewModel.removeOneTag(id: oldTag.last?.id ?? UUID())
+                                }
+                            }
+                            if let lastIndex = localTags.indices.last, lastIndex > 0, localTags[lastIndex].title == "" {
+                                taskViewModel.updateTag(tag: localTags[lastIndex - 1])
                             }
                         }
-                        if let lastIndex = localTags.indices.last, lastIndex > 0, localTags[lastIndex].title == "" {
-                            taskViewModel.updateTag(tag: localTags[lastIndex - 1])
-                        }
-                    }
-            }
-            
-            Section("section_status") {
-                StatusIndicator(status: task.status)
-            }
-            .listRowBackground(Color.clear)
-            
-            Section("section_note") {
-                ZStack {
-                    TextEditor(text: $task.note)
-                        .frame(height: 125)
-                    if task.note.isEmpty {
-                        VStack {
-                            HStack {
-                                Text(taskNote)
-                                    .foregroundStyle(.tertiary)
-                                    .padding(.top, 16)
-                                    .padding(.leading, 5)
-                                
+                }
+                
+                Section("section_note") {
+                    ZStack {
+                        TextEditor(text: $task.note)
+                            .frame(height: 125)
+                        if task.note.isEmpty {
+                            VStack {
+                                HStack {
+                                    Text(taskNote)
+                                        .foregroundStyle(.tertiary)
+                                        .padding(.top, 16)
+                                        .padding(.leading, 5)
+                                    
+                                    Spacer()
+                                }
                                 Spacer()
                             }
-                            Spacer()
                         }
                     }
                 }
             }
+            Button {
+                taskViewModel.toggleTaskStatus(task: task)
+            } label: {
+                StatusIndicator(status: task.status)
+            }
+        }
+        .background(TodoStatus.colorStyle(task.status).opacity(0.20))
+        .scrollContentBackground(.hidden)
+        
+        .onAppear {
+            originalTask = task
         }
         .onChange(of: task) {
             taskViewModel.updateTitleAndNote(with: task)
         }
-        
-        HStack {
-            VStack {
-                Section("section_createAt") {
-                    Text(task.date.format())
-                        .font(.footnote)
-                }
-                .font(.subheadline)
-                .padding(.horizontal, 25)
-            }
-            VStack {
-                Section("section_changeAt") {
-                    Text(task.lastUpdate.format())
-                        .font(.footnote)
-                }
-                .font(.subheadline)
-                .padding(.horizontal, 25)
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                Button(String(localized: "cancel"),
+                       role: .destructive,
+                       action: {
+                    if let originalTask {
+                        task = originalTask
+                        dismiss()
+                    }
+                })
+                    .buttonStyle(.borderedProminent)
             }
         }
-        
-        
-        .listRowBackground(Color.clear)
+        .safeAreaInset(edge: .bottom) {
+            VStack{
+                Divider()
+                HStack {
+                    VStack {
+                        Section("section_createAt") {
+                            Text(task.date.format())
+                                .font(.footnote)
+                        }
+                        .font(.subheadline)
+                        .padding(.horizontal, 25)
+                    }
+                    VStack {
+                        Section("section_changeAt") {
+                            Text(task.lastUpdate.format())
+                                .font(.footnote)
+                        }
+                        .font(.subheadline)
+                        .padding(.horizontal, 25)
+                    }
+                }
+            }
+            .background(.thinMaterial)
+        }
         .scrollIndicators(.hidden)
         .navigationTitle("\(task.title)")
         .navigationBarTitleDisplayMode(.inline)
@@ -100,7 +123,9 @@ struct DetailTaskView: View {
     let tags = [TagLocal(title: "Tag1"), TagLocal(title: "Tag2")]
     let task = TasksLocal(title: "Prueba", date: Date.now, status: .pending, note: "Prueba de nota", lastUpdate: Date())
     
-    return DetailTaskView(task: task, localTags: tags)
-        .environment(taskViewModel)
+    return NavigationView {
+        DetailTaskView(task: task, localTags: tags)
+            .environment(taskViewModel)
+    }
 }
 
